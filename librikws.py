f4f39ba8243c51565cd269401bd228430c754f60
markjosims <mjsimmons@ucsd.edu>
2025-10-07 08:00:04 -0700

    save sample KWS dataset

--- librikws.py
+++ librikws.py
@@ -13,11 +13,13 @@ import pandas as pd
 from tqdm import tqdm
 from typing import *
 from praatio import textgrid
+from random import shuffle
+from tqdm import tqdm
 
 tqdm.pandas()
 
 # input files
-DATASETS = os.environ['DATASETS']
+DATASETS = '/mnt/LocalStorage/mjsimmons/datasets/'
 
 LIBRISPEECH_ROOT = os.path.join(DATASETS, "LibriSpeech")
 LIBRISPEECH_SPLIT = "train-clean-100"
@@ -31,6 +33,11 @@ LIBRIPHRASE_AUDIO = os.path.join(LIBRIPHRASE_DATA, LIBRISPEECH_SPLIT)
 # output files
 SENTENCES_CSV = os.path.join(LIBRIPHRASE_DATA, f"{LIBRISPEECH_SPLIT}_sentences.csv")
 WORDS_CSV = os.path.join(LIBRIPHRASE_DATA, f"{LIBRIPHRASE_SPLIT}_keywords.csv")
+SAMPLE_KWS_CSV = os.path.join(LIBRIPHRASE_DATA, f"{LIBRIPHRASE_SPLIT}_sample_kws_list.csv")
+SAMESPEAKER_KWS_CSV = os.path.join(
+    LIBRIPHRASE_DATA,
+    f"{LIBRIPHRASE_SPLIT}_sample_kws_list_samespeaker.csv"
+)
 
 def get_librispeech_path(relpath: str) -> str:
     """
@@ -81,11 +88,23 @@ def get_librispeech_textgrid(librispeech_relpath: str) -> pd.DataFrame:
     textgrid_path = librispeech_abspath + '.TextGrid'
     return textgrid_to_df(textgrid_path)
     
+def get_keyword_df():
+    return pd.read_csv(WORDS_CSV)
+
+def get_sentence_df():
+    return pd.read_csv(SENTENCES_CSV)
+
+def get_sample_kws_df():
+    return pd.read_csv(SAMPLE_KWS_CSV)
+
+def get_samespeaker_kws_df():
+    return pd.read_csv(SAMESPEAKER_KWS_CSV)
 
 def get_unique_keywords(keyword_df: Optional[pd.DataFrame]=None) -> List[str]:
     if keyword_df is None:
-        keyword_df = pd.read_csv(WORDS_CSV)
+        keyword_df = get_keyword_df()
     return list(keyword_df['keyword'].unique())
+
 
 def get_keyword_tokens(
         keyword: str,
@@ -100,7 +119,7 @@ def get_keyword_tokens(
         pandas.DataFrame, subset of `keyword_df` corresponding to the given keyword
     """
     if keyword_df is None:
-        keyword_df = pd.read_csv(WORDS_CSV)
+        keyword_df = get_keyword_df()
     keyword_mask = keyword_df['keyword']==keyword
     return keyword_df[keyword_mask]
 
@@ -128,18 +147,26 @@ def get_random_keyword_token(
 def get_keyword_sentences(
         keyword: str,
         sentence_df: Optional[pd.DataFrame]=None,
+        num_records: Optional[int]=None,
+        speaker: Optional[str]=None,
     ) -> pd.DataFrame:
     """
     Arguments:
         keyword:        String representing keyword/phrase to query
         sentence_df:    Optional Pandas DataFrame containing sentence data.
                         If not passed, load from $SENTENCES_CSV file.
+        num_records:    int indicating number of rows to return (if None return all)
     Returns:
         pandas.DataFrame, subset of `sentence_df` containing to the given keyword
     """
     if sentence_df is None:
-        sentence_df = pd.read_csv(SENTENCES_CSV)
+        sentence_df = get_sentence_df()
     keyword_mask = sentence_df['sentence'].str.contains(keyword)
+    if speaker:
+        speaker_mask = sentence_df['speaker']==speaker
+        keyword_mask = keyword_mask&speaker_mask
+    if num_records is not None:
+        return sentence_df[keyword_mask].sample(num_records)
     return sentence_df[keyword_mask]
 
 def get_random_keyword_sentence_pair(
@@ -209,3 +236,64 @@ def get_random_negative_keyphrase(
     if num_records==1:
         return rows.iloc[0]
     return rows
+
+def make_sample_kws_dataset(
+        num_keywords: int = 10,
+        num_positive: int = 10,
+        num_negative: int = 20,
+        same_speaker: bool=False,
+):
+    kws_df_list = []
+    keyword_df = get_keyword_df()
+    sentence_df = get_sentence_df()
+    keyword_list = get_unique_keywords()
+    shuffle(keyword_list)
+
+    for _ in tqdm(range(num_keywords)):
+        while True:
+            try:
+                keyword = keyword_list.pop()
+                print(f"Sampling records for keyword '{keyword}'")
+
+                keyword_row = get_random_keyword_token(keyword, keyword_df=keyword_df)
+                keyword_row = keyword_row.to_dict()
+                speaker = None
+                if same_speaker:
+                    speaker = keyword_row['speaker']
+                keyword_filepath = get_libriphrase_audio_path(keyword_row['file'])
+
+                positive_records = get_keyword_sentences(
+                    keyword,
+                    sentence_df,
+                    num_records=num_positive,
+                    speaker=speaker,
+                )
+                positive_records['label']=1
+
+                negative_records = get_random_negative_keyphrase(
+                    keyword,
+                    sentence_df,
+                    num_records=num_negative,
+                    speaker=speaker,
+                    same_speaker=same_speaker,
+                )
+                negative_records['label']=0
+
+                keyword_records = pd.concat([positive_records, negative_records])
+                keyword_records['keyword'] = keyword
+                keyword_records['keyword_file'] = keyword_filepath
+                keyword_records = keyword_records.rename(columns={'file': 'sentence_file'})
+
+                kws_df_list.append(keyword_records)
+                break
+            except ValueError:
+                print(f"Not enough tokens of keyword '{keyword}', trying again")
+    kws_df = pd.concat(kws_df_list)
+    return kws_df
+
+if __name__ == '__main__':
+    kws_df = make_sample_kws_dataset()
+    kws_df.to_csv(SAMPLE_KWS_CSV, index=False)
+
+    samespeaker_df = make_sample_kws_dataset(same_speaker=True)
+    samespeaker_df.to_csv(SAMESPEAKER_KWS_CSV, index=False)
